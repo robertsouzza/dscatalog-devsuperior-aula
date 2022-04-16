5c552052bd1f02b397aec428c6213b4eabe462b2
Roberto Souza <rcsouzza@gmail.com>
2022-04-15 20:35:04 -0400

    UserUpdateValidato

--- backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.RoleDTO;
 import com.devsuperior.dscatalog.dto.UserDTO;
 import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.dto.UserUpdateDTO;
 import com.devsuperior.dscatalog.entities.Role;
 import com.devsuperior.dscatalog.entities.User;
 import com.devsuperior.dscatalog.repositories.RoleRepository;
@@ -61,7 +62,7 @@ public class UserService {
 
 
 	@Transactional
-	public UserDTO update(Long id, UserDTO dto) {
+	public UserDTO update(Long id, UserUpdateDTO dto) {
 		try {
 			User entity = repository.getOne(id); // OBS 02 - não toca no banco de dados vai instanciar um objeto, provisorio, com os dados e esse id daquele objeto, só quando // mandar salvar ai sim que ele vai no banco de dados.
 			copyDtoToEntity(dto, entity);

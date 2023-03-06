148e70273fb18020bc6a366a18e397b400b59109
robertsouzza <rcsouzza@gmail.com>
2023-03-06 15:57:02 -0400

    privateroute

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,15 +1,11 @@
 import { ReactComponent as MainImage } from 'assets/images/main-images.svg';
 import ButtonIcon from 'components/ButtonIcon';
 import { Link } from 'react-router-dom';
-import { isAuthenticated } from 'util/requests';
 import './styles.css';
 
 const Home = () => {
   return (
     <div className="home-container">
-
-      <h1>{isAuthenticated() ? 'autenticado' : 'Não autenticado'}</h1>
-
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
@@ -21,7 +17,7 @@ const Home = () => {
           </div>
           <div>
             <Link to="/products">
-              <ButtonIcon text='Inicie agora a sua busca' />
+              <ButtonIcon text="Inicie agora a sua busca" />
             </Link>
           </div>
         </div>

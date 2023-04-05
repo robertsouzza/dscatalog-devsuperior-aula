9cb6b3b74bc118c4d717c05093936f356a2e9bf7
robertsouzza <rcsouzza@gmail.com>
2023-04-05 16:17:54 -0400

    Redirecting to intendend route after login

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,18 +1,27 @@
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
 import { getTokenData, requestBackendLogin, saveAuthData } from 'util/requests';
-
-import './styles.css';
 import { useContext, useState } from 'react';
 import { AuthContext } from 'AuthContext';
+
+import './styles.css';
+
 
 type FormData = {
   username: string;
   password: string;
 };
 
+type LocationState = {
+  from: string;
+}
+
 const Login = () => {
+
+  const location = useLocation<LocationState>();
+
+  const { from } = location.state || { from: { pathname: '/admin'}}
 
   const {setAuthContextData} = useContext(AuthContext);
 
@@ -32,7 +41,7 @@ const Login = () => {
           tokenData: getTokenData(),
         })
 
-        history.push('/admin');
+        history.replace(from);
       })
       .catch((error) => {
         setHasError(true);

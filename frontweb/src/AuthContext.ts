5651d6a9e6c13456159813cfc596da907a392630
robertsouzza <rcsouzza@gmail.com>
2023-04-05 15:49:47 -0400

    global state with Context API

---
+++ frontweb/src/AuthContext.ts
@@ -0,0 +1,20 @@
+import { createContext } from "react";
+import { TokenData } from "util/requests";
+
+export type AuthContextData = {
+    authenticated: boolean;
+    tokenData?: TokenData; //opcional preencher o token
+};
+
+export type AuthcontextType = {
+    authContextData: AuthContextData,
+    setAuthContextData: (authContextData: AuthContextData) => void;
+};
+
+export const AuthContext = createContext<AuthcontextType>({
+
+    authContextData: {
+        authenticated: false
+    },
+    setAuthContextData: () => null
+});

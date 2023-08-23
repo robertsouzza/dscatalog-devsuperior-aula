d87b37d9ba6a54e9bd1324796b803f574955eddd
robertsouzza <rcsouzza@gmail.com>
2023-08-23 17:38:04 -0400

    Product crud form HTML

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -30,7 +30,7 @@ const List = () => {
 
 
     return (
-        <>
+        <div className="product-crud-container">
         <div className="product-crud-bar-container">
            <Link to="/admin/products/create">
                <button className="btn btn-primary text-white btn-crud-add">ADICIONAR</button> 
@@ -52,7 +52,7 @@ const List = () => {
             </div>
         </div>
 
-        </>
+        </div>
     );
 };
 

14091d30af551a4a0a66e65b8019e267fb1971d1
robertsouzza <rcsouzza@gmail.com>
2023-09-07 17:22:08 -0400

    Editing products

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -2,29 +2,58 @@ import { useForm } from 'react-hook-form';
 import { Product } from 'types/product';
 import { requestBackend } from 'util/requests';
 import { AxiosRequestConfig } from 'axios';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
+
 import './styles.css';
+import { useEffect } from 'react';
 
-
+type UrlParams = {
+   productId: string;
+};
 
 const Form = () => {
 
+   const { productId } = useParams<UrlParams>();
+
+   const isEditing = productId !== 'create';
+
    const history = useHistory();
 
-   const { register, handleSubmit, formState: { errors } } = useForm<Product>();
+   const { register,
+      handleSubmit,
+      formState: { errors },
+      setValue
+   } = useForm<Product>();
+
+   useEffect(() => {
+      if (isEditing) {
+         requestBackend({ url: `/products/${productId}` })
+            .then((response) => {
+
+               const product = response.data as Product;
+
+               setValue('name', product.name);
+               setValue('price', product.price);
+               setValue('description', product.description);
+               setValue('imgUrl', product.imgUrl);
+               setValue('categories', product.categories);
+            });
+      }
+   }, [isEditing, productId, setValue])
 
    const onSubmit = (formData: Product) => {
 
       const data = {
-         ...formData, 
-            imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg",
-            categories: [{id: 1, name: ""}]}
+         ...formData,
+         imgUrl: isEditing ? formData.imgUrl : "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg",
+         categories: isEditing ? formData.categories : [{ id: 1, name: "" }]
+      }
 
       const config: AxiosRequestConfig = {
-         method: 'POST',
-         url: "/products",
+         method: isEditing ? 'PUT' : 'POST',
+         url: isEditing ? `/products/${productId}` : "/products",
          data: data,
-         withCredentials: true
+         withCredentials: true,
       };
 
 
@@ -37,7 +66,7 @@ const Form = () => {
    };
 
    const handleCancel = () => {
-        history.push("/admin/products");
+      history.push("/admin/products");
    }
 
    return (
@@ -74,14 +103,14 @@ const Form = () => {
                   </div>
                   <div className="col-lg-6">
                      <div>
-                        <textarea 
-                            rows={10} 
-                            {...register('description', {
+                        <textarea
+                           rows={10}
+                           {...register('description', {
                               required: 'Campo obrigatório',
                            })}
                            className={`form-control base-input h-auto ${errors.name ? 'is-invalid' : ''}`}
                            placeholder="Descrição"
-                           name="description" 
+                           name="description"
                         />
                         <div className="invalid-feedback d-block">{errors.description?.message}</div>
                      </div>
@@ -89,7 +118,7 @@ const Form = () => {
                </div>
                <div className="product-crud-buttons-container">
                   <button className="btn btn-outline-danger product-crud-button"
-                  onClick={handleCancel}
+                     onClick={handleCancel}
                   >
                      CANCELAR
                   </button>

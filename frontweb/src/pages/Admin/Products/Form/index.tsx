6b0456596a6d4d97092ca3c71c6a0a8cd04873e1
robertsouzza <rcsouzza@gmail.com>
2024-03-18 22:49:33 -0400

    React Select Layout

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -6,12 +6,19 @@ import { useHistory, useParams } from 'react-router-dom';
 
 import './styles.css';
 import { useEffect } from 'react';
+import Select from 'react-select';
 
 type UrlParams = {
    productId: string;
 };
 
 const Form = () => {
+
+   const options = [
+      { value: 'chocolate', label: 'Chocolate' },
+      { value: 'strawberry', label: 'Strawberry' },
+      { value: 'vanilla', label: 'Vanilla' }
+    ]
 
    const { productId } = useParams<UrlParams>();
 
@@ -88,6 +95,22 @@ const Form = () => {
                         />
                         <div className="invalid-feedback d-block">{errors.name?.message}</div>
                      </div>
+                     
+
+                     <div className="margin-bottom-30">
+                        <Select
+                          options={options}
+                          classNamePrefix="product-crud-select"
+                          isMulti
+                        />
+                     </div> 
+                     
+                     
+                     
+                     
+                     
+                     
+                     
                      <div className="margin-bottom-30">
                         <input
                            {...register('price', {

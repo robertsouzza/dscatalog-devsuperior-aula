85863ea08e758490ae21ea398596777f92737acf
robertsouzza <rcsouzza@gmail.com>
2024-11-21 23:00:09 -0400

    Mocking function with spuOn

---
+++ frontweb/src/util/__tests__/auth.spec.ts
@@ -0,0 +1,24 @@
+import { hasAnyRoles } from "../auth";
+import * as TokenModule from '../token';
+
+describe('hasAnyRoles tests', () => {
+
+    test('should return true when empty list', () => {
+        const result = hasAnyRoles([]);
+        expect(result).toEqual(true);
+    });
+
+    test('should return true when user has given role', () => { // ou seja a minha função hasAnyRoles tem quetornar verdadeiro quando o usuário possuir o role que for passado de argumento 
+        
+        jest.spyOn(TokenModule,'getTokenData').mockReturnValue({
+            exp: 0,
+            user_name: 'roberto',
+            authorities: ['ROLE_ADMIN','ROLE_OPERATOR']
+        })
+
+        const result = hasAnyRoles(['ROLE_ADMIN']);              
+        expect(result).toEqual(true);
+    });
+
+});
+

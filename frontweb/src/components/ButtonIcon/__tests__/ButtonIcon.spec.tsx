cf530f3aeadf5854af200ca575b6d9abb696e803
robertsouzza <rcsouzza@gmail.com>
2024-11-27 23:02:00 -0400

    ButtonIcon test

---
+++ frontweb/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import ButtonIcon from "..";
+
+//Deveria reinderizar um botão com testo informado"
+test('ButtonIcon should render button with given text',() => {
+    // ARRANGE
+       const text = "Fazer login";
+    // ACT
+       render(
+            <ButtonIcon text ={text} />
+       );
+      //screen.debug();
+
+    // ASSERT
+       expect(screen.getByText(text)).toBeInTheDocument();
+});

1ce66281414bcad59dff7a40e9673c0cb5446779
robertsouzza <rcsouzza@gmail.com>
2025-04-09 08:59:03 -0400

    Catalog, waitFor, async-await

---
+++ frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -0,0 +1,23 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import Catalog from "..";
+import history from "util/history";
+import {Router}from "react-router-dom";
+
+test('should render Catalog with products', async() => {
+
+       render(
+          <Router history={history} >
+               <Catalog />
+          </Router>
+            
+       );
+
+      // screen.debug();
+       expect(screen.getByText("Catálogo de produtos")).toBeInTheDocument();
+
+       await waitFor(()=>{
+         expect(screen.getByText("Smart TV")).toBeInTheDocument();
+       });
+
+      // screen.debug();
+});
